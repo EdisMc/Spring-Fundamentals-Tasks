c452082c827b0c524bdd54e5b991bebb0439e8be
Edis Reyhan <92871901+EdisMc@users.noreply.github.com>
2022-06-25 20:32:50 +0300

    Create Song.java

---
+++ Exam - 25.06.2022/src/main/java/bg/softuni/springexam/model/Song.java
@@ -0,0 +1,34 @@
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name = "songs")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Song extends BaseEntity {
+
+    @Column(nullable = false, unique = true)
+    private String performer;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private Integer duration;
+
+    @Column
+    private LocalDate releaseDate;
+
+    @ManyToMany(mappedBy = "songs")
+    private List<User> users;
+
+    @ManyToOne
+    private Style style;
+}
